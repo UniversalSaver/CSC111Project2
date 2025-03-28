d744ef26effa241902d9dabeec5eef7ea863f353
Nabhan Rashid <nabhanrashid@TDDoom-4.local>
2025-03-27 21:55:51 -0400

    Added copyright to the main.py, and am considering it DONE

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 """
 The main file, where anything necessary will be run. This will likely be mostly the tkinter call
 
-TODO - add copyright
+Copyright and Usage Information
+===============================
+This file is solely provided for the use in grading and review of the named student's
+work by the TAs and Professors of CSC111. All further distribution of this code whether
+as is or modified is firmly prohibited.
+
+This file is Copyright (c) Nabhan Rashid, Danny Tran, and Tai Poole
+
 """
 import gui_interface
 
 if __name__ == '__main__':
-    #TODO something to do with choosing the data? im not sure
     app = gui_interface.App("./data_files/actors_and_movies.db")
     app.run()
